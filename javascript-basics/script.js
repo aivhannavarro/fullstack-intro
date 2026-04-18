2637210356da3e33882486ed3d0854e899d4937e
aivhannavarro <aivhannavarro14@gmail.com>
2026-04-18 14:42:30 +0800

    Add JavaScript basics lab

---
+++ javascript-basics/script.js
@@ -0,0 +1,13 @@
+var name = "aivhan";
+let age = 20;
+const gender = "male";
+let pass = true;
+
+console.log(name);
+console.log(age);
+console.log(gender);
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof gender);
+console.log(typeof pass);
